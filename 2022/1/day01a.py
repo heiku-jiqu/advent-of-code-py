98104367c1e2e82199b7b32e42fb798b1c331c0f
Heiser <heiser9295@hotmail.com>
2023-01-04 17:45:49 +0800

    solve day1a

---
+++ 2022/1/day01a.py
@@ -0,0 +1,24 @@
+filepath = "input.txt"
+with open(filepath, "r", encoding="utf-8") as f:
+    lines = f.readlines()
+
+
+def split_list(l: list[str], s: str) -> list:
+    output = []
+    sublist = []
+    for item in l:
+        if item == s:
+            output.append(sublist)
+            sublist = []
+        else:
+            sublist.append(item)
+    return output
+
+
+grouped_list = split_list([line.rstrip() for line in lines], "")
+grouped_int_list = [[int(x) for x in l] for l in grouped_list]
+sum_list = [sum(l) for l in grouped_int_list]
+output = max(sum_list)
+
+print(output)
+
