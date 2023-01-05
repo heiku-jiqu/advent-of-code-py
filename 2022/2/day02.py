775a850ce525da78a301e4e281c92a2d536e3298
Heiser <heiser9295@hotmail.com>
2023-01-05 17:29:00 +0800

    complete day 2 part 2

--- 2022/2/day02.py
+++ 2022/2/day02.py
@@ -20,6 +20,18 @@ class PlayerSymbols(Enum):
     Z = HandShape.Scissors
 
 
+class RoundResult(Enum):
+    WIN = 6
+    DRAW = 3
+    LOSE = 0
+
+
+class RoundSymbols(Enum):
+    X = RoundResult.LOSE
+    Y = RoundResult.DRAW
+    Z = RoundResult.WIN
+
+
 def score_round(round: Tuple[OpponentSymbols, PlayerSymbols]):
     if round[1].value == round[0].value:
         result = 3
@@ -41,6 +53,28 @@ def score_round(round: Tuple[OpponentSymbols, PlayerSymbols]):
     return result + round[1].value.value
 
 
+def score_round_part2(round: Tuple[OpponentSymbols, RoundSymbols]):
+    match round[1].value:
+        case RoundResult.DRAW:
+            player_shape = round[0].value
+        case RoundResult.WIN:
+            if round[0].value == HandShape.Rock:
+                player_shape = HandShape.Paper
+            if round[0].value == HandShape.Paper:
+                player_shape = HandShape.Scissors
+            if round[0].value == HandShape.Scissors:
+                player_shape = HandShape.Rock
+        case RoundResult.LOSE:
+            if round[0].value == HandShape.Rock:
+                player_shape = HandShape.Scissors
+            if round[0].value == HandShape.Paper:
+                player_shape = HandShape.Rock
+            if round[0].value == HandShape.Scissors:
+                player_shape = HandShape.Paper
+
+    return round[1].value.value + player_shape.value
+
+
 with open("input.txt") as f:
     lines = f.read()
 
@@ -48,6 +82,11 @@ rounds = [x for x in lines.split("\n") if x != ""]
 rounds_list = [tuple(r.split(" ")) for r in rounds]
 rounds_symbols = [(OpponentSymbols[x[0]], PlayerSymbols[x[1]]) for x in rounds_list]
 
-answer = sum([score_round(x) for x in rounds_symbols])
+answer_part1 = sum([score_round(x) for x in rounds_symbols])
 
-print(f"Part 1: {answer}")
+rounds_symbols_part2 = [(OpponentSymbols[x[0]], RoundSymbols[x[1]]) for x in rounds_list]
+
+answer_part2 = sum([score_round_part2(x) for x in rounds_symbols_part2])
+
+print(f"Part 1: {answer_part1}")
+print(f"Part 2: {answer_part2}")
