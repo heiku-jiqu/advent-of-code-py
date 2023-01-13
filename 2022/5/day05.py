1eae08b9514b79e29f0522a38339a1d64f47b98b
Heiser <heiser9295@hotmail.com>
2023-01-13 20:21:14 +0800

    solve day 5 part 1

--- 2022/5/day05.py
+++ 2022/5/day05.py
@@ -1,3 +1,6 @@
+from re import findall
+from dataclasses import dataclass
+
 with open("input.txt") as f:
     input = [s.rstrip("\n") for s in f.readlines()]
 
@@ -14,5 +17,37 @@ def parse_input_stacks(input: list[str]) -> list[list[str]]:
     return output
 
 
+@dataclass
+class Move:
+    from_stack: int
+    to_stack: int
+    num_move: int
+
+
+def parse_moves(input: list[str]) -> list:
+    output = []
+    for s in input:
+        i_list = findall(r"\d+", s)
+        i_list = [int(i) for i in i_list]
+        output.append(
+            Move(num_move=i_list[0], from_stack=i_list[1], to_stack=i_list[2])
+        )
+    return output
+
+
+def do_move(stacks, move: Move):
+    stacks[move.to_stack - 1].append(stacks[move.from_stack - 1].pop())
+    if move.num_move == 1:
+        return None
+    else:
+        new_move = move
+        new_move.num_move = new_move.num_move - 1
+        do_move(stacks, new_move)
+
+
 start_stacks = parse_input_stacks(input_start_stacks)
-print(start_stacks)
+parsed_moves = parse_moves(input_move_list)
+for move in parsed_moves:
+    do_move(start_stacks, move)
+part1_answer = "".join([s[-1] for s in start_stacks])
+print(f"Part 1: {part1_answer}")
