b2d40a4070d187953ed87fc6da4b9bb1ad964bcc
Heiser <heiser9295@hotmail.com>
2023-01-13 20:00:08 +0800

    parse input stacks

--- 2022/5/day05.py
+++ 2022/5/day05.py
@@ -1,8 +1,18 @@
 with open("input.txt") as f:
-    input = [s.strip() for s in f.readlines()]
+    input = [s.rstrip("\n") for s in f.readlines()]
+
+input_start_stacks = input[: input.index("") - 1]
+input_move_list = input[input.index("") + 1 :]
 
 
-print(input[: input.index("")])
-print("               \n\n")
-print(input[input.index("") + 1 :])
+def parse_input_stacks(input: list[str]) -> list[list[str]]:
+    output = []
+    for i, loc in enumerate(range(1, 35, 4)):
+        temp = [s[loc] for s in input_start_stacks]
+        temp.reverse()
+        output.append([crate for crate in temp if crate != " "])
+    return output
 
+
+start_stacks = parse_input_stacks(input_start_stacks)
+print(start_stacks)
