8a9ab5bc6a8da4688746810713482437e998cbe8
Heiser <heiser9295@hotmail.com>
2023-02-03 12:19:08 +0800

    complete day 7

--- 2022/7/day07.py
+++ 2022/7/day07.py
@@ -1,38 +1,4 @@
-from __future__ import annotations
-from dataclasses import dataclass
 from collections import defaultdict
-
-
-@dataclass
-class File:
-    name: str
-    size: int
-
-
-class Directory:
-    def __init__(self, name=None, parent_dir=None):
-        self.name = name
-        self.parent_dir = parent_dir
-        self.child_dirs = list()
-        self.files = list()
-        self.size = 0
-
-    def add_child_dir(self, child_dir: Directory):
-        self.child_dirs.append(child_dir)
-
-    def add_file(self, f: File):
-        self.files.append(f)
-
-    def get_filesize(self):
-        return sum(s.size for s in self.files)
-
-    def get_dirsize(self, fs):
-        if len(self.child_dirs) == 0:
-            return self.get_filesize()
-        else:
-            return self.get_filesize + sum(
-                fs[child].get_dirsize(fs) for child in self.child_dirs
-            )
 
 
 def main():
@@ -40,45 +6,46 @@ def main():
         input = f.read()
     input_split = input.split("$ ")
     commands = [[c for c in i.split("\n") if c] for i in input_split]
-    print(commands)
 
-    def dict_factory():
-        return Directory()
-
-    filesys = defaultdict(dict_factory)
+    filesys = defaultdict(int)
     current_dir = None
+
     for c in commands:
-        print(c)
         if len(c) == 0:
             continue
         if c[0].startswith("cd"):
             if c[0] == "cd ..":
-                current_dir = filesys[current_dir.parent_dir]
-                # print(current_dir.name)
-            if c[0] == "cd /":
-                current_dir = filesys["/"]
-                current_dir.name = "/"
-                # print(current_dir.name)
+                current_dir = current_dir[: current_dir.rfind("/")]
+            elif c[0] == "cd /":
+                current_dir = "/"
             else:
                 name = c[0][3:]
-                current_dir = filesys[name]
-                current_dir.name = name
-                # print(current_dir.name)
+                current_dir = current_dir + "/" + name
         if c[0].startswith("ls"):
             for item in c[1:]:
                 if item.startswith("dir"):
-                    current_dir.add_child_dir(item[4:])
-                    # print(item[4:])
+                    pass
                 else:
                     size, filename = item.split(" ")
                     size = int(size)
-                    current_dir.add_file(File(name=filename, size=size))
-                    # print(f"size: {size}, filename: {filename}")
-    # dir = Directory("/")
-    # dir.files = [File("abc", 100), File("def", 202)]
-    # print(dir.get_filesize())
-    part1_dirs = [k for k, v in filesys.items() if v.get_dirsize(filesys) <= 100000]
-    print(part1_dirs)
+                    update_dir = current_dir
+                    for i in range(current_dir.count("/")):
+                        filesys[update_dir] += size
+                        update_dir = update_dir[: update_dir.rfind("/")]
+
+    part1_dirs_size = [v for k, v in filesys.items() if v <= 100000]
+    print(f"Part 1: {sum(part1_dirs_size)}")
+
+    total_disk_space = 70000000
+    disk_used = filesys["/"]
+    space_needed = 30000000
+
+    eligible_dirs = [
+        v
+        for k, v in filesys.items()
+        if total_disk_space - (disk_used - v) > space_needed
+    ]
+    print(f"Part 2: {min(eligible_dirs)}")
 
 
 if __name__ == "__main__":
