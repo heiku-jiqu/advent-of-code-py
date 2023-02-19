e8556a51c99e0479b2c7bdb3c5a7484df71c844c
Heiser <heiser9295@hotmail.com>
2023-02-19 12:55:39 +0800

    add parse_test

--- 2022/11/day11.py
+++ 2022/11/day11.py
@@ -7,7 +7,7 @@ class Monkey:
     def __init__(self) -> None:
         self.items: List[int]
         self.operation: Callable
-        self.test: Callable[[], int]
+        self.test: Callable[[int], int]
 
     def parse_items(self, s: str) -> Self:
         split_string = s.replace("Starting items: ", "").split(", ")
@@ -33,6 +33,15 @@ class Monkey:
                     self.operation = lambda x: x * number
                 case _:
                     raise Exception('Unknown Operation')
+        return self
+    
+    def parse_test(self, test_str: str, true_str: str, false_str: str) -> Self:
+        divisor = int(test_str.split(' ')[-1])
+        true_monkey = int(true_str.split(' ')[-1])
+        false_monkey = int(false_str.split(' ')[-1])
+        self.test = lambda worry: true_monkey if worry % divisor == 0 else false_monkey
+
+        return self
 
 
 def parse_input(input: str) -> List[List[str]]:
@@ -67,8 +76,19 @@ class TestMonkey(TestCase):
         m.parse_operation(op_string)
         self.assertEqual(m.operation(2), 2 + 19)
         
-    def test_parse_items(self):
+    def test_parse_items_old(self):
         m = Monkey()
         op_string = "Operation: new = old * old"
         m.parse_operation(op_string)
         self.assertEqual(m.operation(2), 4)
+
+    def test_parse_test(self):
+        m = Monkey()
+        test_str = "Test: divisible by 17"
+        true_str = "    If true: throw to monkey 0"
+        false_str = "    If false: throw to monkey 1"
+        m.parse_test(test_str, true_str, false_str)
+        # 17 divisible, so throw to monkey 0
+        self.assertEqual(m.test(17), 0)
+        # 18 not divisible, so throw to monkey 1
+        self.assertEqual(m.test(18), 1)
