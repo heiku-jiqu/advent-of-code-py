8cff840f99879e25e9dfd06b748c3bac8d55cb18
Heiser <heiser9295@hotmail.com>
2023-02-21 18:56:06 +0800

    solve day 12 part 1

--- 2022/12/day12.py
+++ 2022/12/day12.py
@@ -2,6 +2,7 @@ from string import ascii_lowercase
 from dataclasses import dataclass, field
 from unittest import TestCase
 from math import inf
+from operator import attrgetter
 
 @dataclass
 class Node:
@@ -9,16 +10,16 @@ class Node:
     letter: str
     height: int = field(init = False)
     neighbour_pos: dict[str, tuple[int, int]] = field(init = False)
-    neighbour_height: dict[str, int] = field(init = False)
+    #neighbour_height: dict[str, int] = field(init = False)
     distance: int = field(init = False)
-    elevation_dict: dict[str, int] = field(init=False)
+    elevation_dict: dict[str, int] = field(init=False, repr=False)
 
     def __post_init__(self):
+        self.elevation_dict = {letter: i for i, letter in enumerate(ascii_lowercase)}
         self.set_height()
         self.set_neighbour_pos()
-        self.set_neighbour_height()
-        self.elevation_dict = {letter: i for i, letter in enumerate(ascii_lowercase)}
-        self.distance = inf
+        
+        self.distance = 999999
 
     def set_height(self):
         match self.letter:
@@ -26,9 +27,9 @@ class Node:
                 self.height = 0
                 self.distance = 0
             case 'E':
-                self.height = len(elevation_dict) - 1
-            case _ if self.letter in elevation_dict:
-                self.height = elevation_dict[self.letter]
+                self.height = len(self.elevation_dict) - 1
+            case _ if self.letter in self.elevation_dict:
+                self.height = self.elevation_dict[self.letter]
             case _:
                 raise Exception("Unknown letter")
 
@@ -41,9 +42,48 @@ class Node:
 if __name__ == "__main__":
     with open("input.txt") as f:
         input = [line.strip() for line in f.readlines()]
-    print(input)
 
-    elevation_dict = {letter: i for i, letter in enumerate(ascii_lowercase)}
+    queue = dict()
+    previous_node = dict()
+    start_pos: tuple
+    end_pos: tuple
+    for row, line in enumerate(input):
+        for col, char in enumerate(line):
+            if char == 'S':
+                start_pos = (row, col)
+            if char == 'E':
+                end_pos = (row, col)
+            queue[(row, col)] = Node((row, col), char)
+
+    queue[start_pos].distance = 0
+
+    while len(queue) > 0:
+        min_dist_node_key = min(queue, key=lambda x: queue.get(x).distance)
+        min_dist_node = queue.pop(min_dist_node_key )
+
+        for key in min_dist_node.neighbour_pos.values():
+            # if key == end_pos:
+                # print('breaking while loop')
+            if key in queue:
+                neighbor = queue[key]
+                if neighbor.height <= min_dist_node.height + 1:
+                    current_distance = min_dist_node.distance + 1
+                    if current_distance < neighbor.distance:
+                        neighbor.distance = current_distance
+                        previous_node[key] = min_dist_node_key
+
+    # print(previous_node)
+    trace_node = previous_node[end_pos]
+    length_of_path = 0
+    while True:
+        length_of_path += 1 
+        try:
+           trace_node = previous_node[trace_node]
+        except KeyError:
+            break
+    
+    print(f"Part 1: {length_of_path}")
+
 
 class TestNode(TestCase):
     def setUp(self) -> None:
