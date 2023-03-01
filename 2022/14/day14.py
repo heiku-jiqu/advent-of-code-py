1a16f2eaa868c76879bdb7c82becf52a6e82797c
heiser <e0031022@u.nus.edu>
2023-03-01 12:04:45 +0800

    add day 14 draft

---
+++ 2022/14/day14.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Coord:
+    x: int
+    y: int
+
+
+# parse rocks path into full coords
+# Grid class with accessor of tile?
+# Find bounding box of all rocks
+#
+
